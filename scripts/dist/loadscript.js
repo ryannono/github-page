276a056feb62d82c4598c9be58de79b7516fa9de
Ryan Nono <79291357+ryannono@users.noreply.github.com>
2022-12-09 12:26:44 -0500

    import & exports

--- scripts/dist/loadscript.js
+++ scripts/dist/loadscript.js
@@ -1,12 +1,15 @@
 "use strict";
-const darkThemePreference = window.matchMedia("(prefers-color-scheme: dark)");
-let darkmodeState = localStorage.getItem("darkmode");
-let currentTheme = "light";
-if ((darkmodeState === "dark system preference" || darkmodeState === "enabled")
-    || (darkThemePreference.matches && darkmodeState != "disabled")) {
-    let style = document.getElementById("lightmode");
-    style.href = "styles/darkmode.css";
-    style.id = "darkmode";
-    currentTheme = "dark";
-    localStorage.setItem("darkmode", "dark system preference");
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.currentTheme = void 0;
+const darkThemePreference = window.matchMedia('(prefers-color-scheme: dark)');
+const darkmodeState = localStorage.getItem('darkmode');
+exports.currentTheme = 'light';
+if (darkmodeState === 'dark system preference' ||
+    darkmodeState === 'enabled' ||
+    (darkThemePreference.matches && darkmodeState !== 'disabled')) {
+    const style = document.getElementById('lightmode');
+    style.href = 'styles/darkmode.css';
+    style.id = 'darkmode';
+    exports.currentTheme = 'dark';
+    localStorage.setItem('darkmode', 'dark system preference');
 }
--- scripts/dist/script.js
+++ scripts/dist/script.js
@@ -1,72 +1,77 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const loadscript_1 = require("./loadscript");
 function swapToggles(themeToMatch) {
-    let darkmodeToggle = document.getElementById("dark");
-    let lightmodeToggle = document.getElementById("light");
-    let toggleText = document.getElementById("toggle_text");
-    if (themeToMatch === "dark") {
-        darkmodeToggle.src = "Images/DarkMode-on.svg";
-        lightmodeToggle.src = "Images/LightMode-off.svg";
-        toggleText.textContent = "Toggle to light mode";
+    const darkmodeToggle = document.getElementById('dark');
+    const lightmodeToggle = document.getElementById('light');
+    const toggleText = document.getElementById('toggle_text');
+    if (themeToMatch === 'dark') {
+        darkmodeToggle.src = 'Images/DarkMode-on.svg';
+        lightmodeToggle.src = 'Images/LightMode-off.svg';
+        toggleText.textContent = 'Toggle to light mode';
     }
     else {
-        darkmodeToggle.src = "Images/DarkMode-off.svg";
-        lightmodeToggle.src = "Images/LightMode-on.svg";
-        toggleText.textContent = "Toggle to dark mode";
+        darkmodeToggle.src = 'Images/DarkMode-off.svg';
+        lightmodeToggle.src = 'Images/LightMode-on.svg';
+        toggleText.textContent = 'Toggle to dark mode';
     }
 }
 function swapToLight() {
-    let style = document.getElementById("darkmode");
-    style.href = "styles/lightmode.css";
-    style.id = "lightmode";
-    localStorage.setItem("darkmode", "disabled");
-    currentTheme = "light";
-    swapToggles(currentTheme);
+    const style = document.getElementById('darkmode');
+    style.href = 'styles/lightmode.css';
+    style.id = 'lightmode';
+    localStorage.setItem('darkmode', 'disabled');
+    theme = 'light';
+    swapToggles(theme);
 }
 function swapToDark() {
-    let style = document.getElementById("lightmode");
-    style.href = "styles/darkmode.css";
-    style.id = "darkmode";
-    localStorage.setItem("darkmode", "enabled");
-    currentTheme = "dark";
-    swapToggles(currentTheme);
+    const style = document.getElementById('lightmode');
+    style.href = 'styles/darkmode.css';
+    style.id = 'darkmode';
+    localStorage.setItem('darkmode', 'enabled');
+    theme = 'dark';
+    swapToggles(theme);
 }
 function themeSwap(trigger) {
-    if (trigger.id === "dark" && currentTheme === "light") {
+    if (trigger.id === 'dark' && theme === 'light') {
         swapToDark();
     }
-    else if (trigger.id === "light" && currentTheme === "dark") {
+    else if (trigger.id === 'light' && theme === 'dark') {
         swapToLight();
     }
 }
 function addClassListener(className, eventName, funtionToExecute) {
-    let targetClass = document.getElementsByClassName(className);
+    const targetClass = document.getElementsByClassName(className);
     for (let i = 0; i < targetClass.length; i++) {
         targetClass[i].addEventListener(eventName, () => funtionToExecute(targetClass[i]));
     }
 }
 function swapProfilePic() {
-    let resizedView = window.matchMedia("(min-width: 1240px)");
-    let profilePic = document.getElementById("profile_pic");
+    const resizedView = window.matchMedia('(min-width: 1240px)');
+    const profilePic = document.getElementById('profile_pic');
     if (resizedView.matches) {
-        profilePic.src = "Images/Profile_Picture.svg";
+        profilePic.src = 'Images/Profile_Picture.svg';
     }
     else {
-        profilePic.src = "Images/Profile_Picture_Mobile.png";
+        profilePic.src = 'Images/Profile_Picture_Mobile.png';
     }
 }
+let theme = loadscript_1.currentTheme;
 function themedCVDownload() {
-    if (currentTheme === "dark") {
-        downloadButton.href = "https://github.com/ryannono/Resume/raw/main/Ryan-Nono-Resume-Winter2023-Dark_compressed.pdf";
+    if (theme === 'dark') {
+        downloadButton.href =
+            'https://github.com/ryannono/Resume/raw/main/Ryan-Nono-Resume-Winter2023-Dark_compressed.pdf';
     }
     else {
-        downloadButton.href = "https://github.com/ryannono/Resume/raw/main/Ryan-Nono-Resume-Winter2023-Light_compressed.pdf";
+        downloadButton.href =
+            'https://github.com/ryannono/Resume/raw/main/Ryan-Nono-Resume-Winter2023-Light_compressed.pdf';
     }
 }
 swapProfilePic();
-window.addEventListener("resize", swapProfilePic);
-if (currentTheme === "dark") {
-    swapToggles(currentTheme);
+window.addEventListener('resize', swapProfilePic);
+if (theme === 'dark') {
+    swapToggles(theme);
 }
-addClassListener("mode_toggle", "click", themeSwap);
-let downloadButton = document.getElementById("resume_button");
-downloadButton.addEventListener("click", themedCVDownload);
+addClassListener('mode_toggle', 'click', themeSwap);
+const downloadButton = document.getElementById('resume_button');
+downloadButton.addEventListener('click', themedCVDownload);
